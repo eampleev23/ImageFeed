ca6a0f65f4ded7fbdf1096e511f97f4ab76ab7e8
ampleev.com <e@mpleev.com>
2025-10-12 08:18:26 +0300

    layout final

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Евгений Амплеев on 12.10.2025.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+}
