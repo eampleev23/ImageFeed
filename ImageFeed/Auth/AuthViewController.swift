5f174f4b04ab90c3187503c20bc1a3444736de98
ampleev.com <e@mpleev.com>
2025-10-16 14:37:52 +0300

    add AuthViewController

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,12 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Евгений Амплеев on 16.10.2025.
+//
+
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+}
