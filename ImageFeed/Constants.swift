7d26ba19c5e4cf90a1076bfae12c4ced913fbbee
ampleev.com <e@mpleev.com>
2025-10-16 12:06:43 +0300

    gitignore fix for private api keys

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Евгений Амплеев on 16.10.2025.
+//
+
+import Foundation
+
+enum Constants {
+    static let accessKey: String = "X-1eXvB7L_d_xxtcNznixBqMP1iAY1_5uqxsowou_Ps"
+    static let secretKey: String = "atBFyx3l0uQtulX81FccgPgSRT1StNUgX7gIBTvtIzQ"
+    static let redirectURI: String = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope: String = "public+read_user+write_likes"
+    static let defaultBaseURL: URL = URL(string: "https://api.unsplash.com")!
+}
+
