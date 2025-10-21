06f3129b77d2fe2770326bc9bd1caea28f2132fd
ampleev.com <e@mpleev.com>
2025-10-21 10:55:52 +0300

    fix backgrcolor for tabbar and splash controllers view

--- ImageFeed/Splash/SplashViewController.swift
+++ ImageFeed/Splash/SplashViewController.swift
@@ -61,6 +61,7 @@ final class SplashViewController: UIViewController, AuthViewControllerDelegate {
         // Создаем экземпляр нужного контроллера из Storyboard
         let tabBarController = UIStoryboard(name: "Main", bundle: .main)
             .instantiateViewController(withIdentifier: "TabBarViewController")
+        tabBarController.view.backgroundColor = .ypBlack
         
         // Установим в `rootViewController` полученный контроллер
         window.rootViewController = tabBarController
